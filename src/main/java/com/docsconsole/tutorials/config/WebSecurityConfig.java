24a653d3a30964049a484478bd64c0d61cd4a7fd
AmarSivas <amarsivas.b@gmail.com>
2021-01-16 21:55:26 +0530

    Revert "added changes for config"
    This reverts commit b6e06bd0aa0f105bcec48beeb48b6a1986c93005.

--- src/main/java/com/docsconsole/tutorials/config/WebSecurityConfig.java
+++ src/main/java/com/docsconsole/tutorials/config/WebSecurityConfig.java
@@ -10,37 +10,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.DataSource;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    DataSource dataSource;
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Autowired
     public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-       /* auth.inMemoryAuthentication()
+        auth.inMemoryAuthentication()
                 .withUser("user").password(passwordEncoder().encode("userpass")).roles("USER")
                 .and()
-                .withUser("admin").password(passwordEncoder().encode("adminpass")).roles("ADMIN");*/
-
-        auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(bCryptPasswordEncoder())
-                .usersByUsernameQuery("SELECT U.USER_NAME, U.USER_PASSWORD, U.USER_ENABLED FROM APP_USERS U WHERE U.USER_NAME= ?")
-                .authoritiesByUsernameQuery("SELECT U.USER_NAME, R.ROLE_NAME FROM APP_USERS U JOIN APP_USER_ROLES UR ON U.USER_ID = UR.USER_ID JOIN APP_ROLES R ON UR.ROLE_ID = R.ROLE_ID WHERE U.USER_NAME = ?");
+                .withUser("admin").password(passwordEncoder().encode("adminpass")).roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/admin").access("hasRole('ROLE_SUPER_ADMIN')")
-                .antMatchers("/home").access("hasAnyRole('ROLE_USER', 'ROLE_SUPER_ADMIN')")
+                .antMatchers("/admin").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/home").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
                 .and()
                 .formLogin().loginPage("/login")
                 .defaultSuccessUrl("/home")
@@ -51,6 +38,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .exceptionHandling().accessDeniedPage("/accessDenied");
 
+        /*http.authorizeRequests()
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .and()
+                .formLogin().loginPage("/login").failureUrl("/login?error")
+                .usernameParameter("username").passwordParameter("password")
+                .and()
+                .logout().logoutSuccessUrl("/login?logout")
+                .and()
+                .exceptionHandling().accessDeniedPage("/accessDenied")
+                .and()
+                .csrf();*/
 
     }
 
